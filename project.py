4eb1f73aec42ee082efe4595ed6869c5c800e09f
Glow-Squid <thecoolersquid@gmail.com>
2021-09-01 08:27:39 +0400

    Update project.py

--- project.py
+++ project.py
@@ -1,22 +1,31 @@
 print("Importing Libraries..")
 import random
+from numpy import select
 import pandas as pd
 import matplotlib.pyplot as pl
 import mysql.connector as myconn
 info = pd.read_csv('Pokemons.csv')
 df = pd.DataFrame(info)
 print("MySQL:Connecting to Database")
-sql=myconn.connect(host="localhost",user="root",passwd="12345678",database="project")
-mycon=sql.cursor()
-if sql.is_connected():
-    print("MySQL: Connected")
-else:
-    print("MySQL: Unable to connect")
-
+try:
+    sql=myconn.connect(host="localhost",user="root",passwd="12345678",database="project")
+    mycon=sql.cursor()
+    if sql.is_connected():
+        print("MySQL: Connected")
+    else:
+        print("MySQL: Unable to connect")
+except:
+    print("MySQL: Connection Error")
+    print("Check your Mysql Config.")
+    quit()
 def Startup():
-    print("Welcome")
+    print("==========Pokemones==========")
     print("1.Login\n2.Signup")
-    r=int(input())
+    try:
+        r=int(input("Select an option: "))
+    except:
+        print("Expected an integer.")
+        Startup()
     if r==2:
         Signup()
 def coin():
@@ -27,12 +36,9 @@ def coin():
     return(mycoins)
 def Signup():
     print("******Signup******")
-    print("Create a User_name")
-    n_user=input()
-    print("Create Password")
-    n_passwd=input()
-    print("Confirm Password")
-    c_passwd=input()
+    n_user=input("Create a User_name: ")
+    n_passwd=input("Create Password: ")
+    c_passwd=input("Confirm Password: ")
     if len(n_passwd)<8:
         print("Password must contain atleast 8 characters.")
     elif n_passwd!=c_passwd:
@@ -49,13 +55,10 @@ def Signup():
             print("User_name already taken.\nTry again")
 def Login():
     print("*******Login*******")
-    print("Enter Username")
-    user=input()
-    print("Enter Password")
-    passwd=input()
+    user=input("Enter Username: ")
+    passwd=input("Enter Password: ")
     mycon.execute(f"select password from users where User_name='{user}';")
     mypassd=mycon.fetchone()
-    
     try:
         for i in mypassd:
             mypass=i
@@ -69,14 +72,18 @@ def Login():
         print("Error:Account Doesn't Exist")
         Login()
 def Main():
-    print(f"*****welcome {user}*****")
+    print("===========Main Menu===========")
     print("Choose an option from below to Continue:")
     print("1.Take a Draw for pokemones.")
     print("2.View your pokemones.")#m_stephin
     print("3.Match.")
     print("4.Check my balance.")#JJ
     print("5.Close")
-    response=int(input())
+    try:
+        response=int(input("Select an option: "))
+    except:
+        print("Expected an integer.")
+        Main()
     if response==1:
         random_poki()
         Main()
@@ -93,7 +100,7 @@ def Main():
     if response ==5:
         quit()
 def random_poki():
-    print("******Pokemone_Draw******")
+    print("=======Pokemone_Draw=======")
     print(f"Your Coins:{mycoin}")
     print("Cost per draw: 1 coin")
     print("Enter the number of draw u would like to take.")
@@ -137,7 +144,7 @@ def random_poki():
         sql.commit()
     Main()
 def Pokemones():
-    print("*******View Pokemones*******")
+    print("===========View Pokemones===========")
     print("1.See names of all your pokemones.")
     print("2.See names of all your Legendery Pokemones")
     print("3.see full details of all your pokemones")
@@ -155,12 +162,13 @@ def Pokemones():
         print(pokemones)
         Main()
 def Match():
-    print("**********Match**********")
-    print("1.Practice")
+    print("===========Match===========")
+    print("1.Practice - Free")
     print("2.Match - Entry fee :2,double back after win")
     print("3.Match -Hard Mode- Entry fee :5, double back after win")
-def base_match():
-    print("uiy")
+def Practice_Match():
+    print("===========Practice===========")
+    print("Select your pokemone:")
 
 Startup()
 user=Login()
